2fa23e56c3d20c28b88dfd49f3cb6cc505479625
Suleiman Mamman A <suleimamman@gmail.com>
2018-11-03 22:46:47 +0100

    Templating for view (PUG) installed

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require("express");
+const path = require("path");
 
+
+// Init App
 const app = express();
+
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
+app.get('/', (req, res) =>{
+	res.send('Hello World');
+});
+
+
+//Start Server
+app.listen(3000, function(){
+  console.log('Server started on port 3000....');
+});
