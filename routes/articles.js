a57d936e298cc740b734f205578400d02e2ea33f
Suleiman Mamman A <suleimamman@gmail.com>
2018-11-14 15:53:26 +0100

    User Access Control implemented

--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 //Bring in Article Models
 let articleTable = require('../models/article');
+//Bring in User Model
+let User = require('../models/user');
 
 //Add article Route
-router.get('/addproduct', (req, res) =>{
+router.get('/addproduct', ensureAuthenticated, (req, res) =>{
 	let errors = {};
 	res.render('saveproduct',{errors:errors});
 	
@@ -14,7 +16,7 @@ router.get('/addproduct', (req, res) =>{
 //Save new article Route
 router.post('/addproduct', (req, res) =>{
   req.checkBody('title','Title is required').notEmpty();
-  req.checkBody('author','Author is required').notEmpty();
+  //req.checkBody('author','Author is required').notEmpty();
   req.checkBody('body','Body is required').notEmpty();
 
   //Get Errors
@@ -25,7 +27,8 @@ router.post('/addproduct', (req, res) =>{
   }else{
   	let article = new articleTable();
 	  article.title = req.body.title;
-	  article.author = req.body.author;
+	  //article.author = req.body.author;
+	  article.author = req.user._id;
 	  article.body = req.body.body;
 
 	  article.save( (err)=>{
@@ -41,20 +44,41 @@ router.post('/addproduct', (req, res) =>{
 
 });
 //View full article Route
-router.get('/fullview/:id', (req, res) =>{
+// router.get('/fullview/:id', (req, res) =>{
 	
- 	articleTable.findById(req.params.id, (err, article) =>{
- 		if (err) {
-	      console.log(err);
-	      return;
-	    }else {
-	     res.render('article', {article:article});
-	    }
+//  	articleTable.findById(req.params.id, (err, article) =>{
+//  		if (err) {
+// 	      console.log(err);
+// 	      return;
+// 	    }else {
+// 	     res.render('article', {article:article});
+// 	    }
  		
- 	}); 
+//  	}); 
 	
 	
+// });
+router.get('/fullview/:id', (req, res)=>{
+
+  articleTable.findById(req.params.id, (err, article)=>{
+
+    User.findById(article.author, (err, user)=>{
+      res.render('article', {article:article, author: user.name});
+    });
+
+  });
+
 });
+// Access Control
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/users/login');
+  }
+}
+
 //Edit article Route
 router.get('/edit/article/:id', (req, res) =>{
 	
