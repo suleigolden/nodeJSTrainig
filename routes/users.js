7abf38ba2a1c608e10a6627682f722a00358e46d
Suleiman Mamman A <suleimamman@gmail.com>
2018-11-14 11:42:35 +0100

    Register process implemented and login page and form created

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,56 @@ router.get('/register', (req, res)=>{
   let errors = {};
   res.render('register', {errors:errors});
 });
+// Register Proccess
+router.post('/register', function(req, res){
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmpassword;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('confirmpassword', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('register',{errors:errors});
+  } else {
+    let newUser = new User({
+      name:name,
+      email:email,
+      username:username,
+      password:password
+    });
+
+    bcrypt.genSalt(10, (err, salt)=>{
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success','You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+  }
+});
+
+// Login Form Route
+router.get('/login', function(req, res){
+  res.render('login');
+});
 
 module.exports = router;
